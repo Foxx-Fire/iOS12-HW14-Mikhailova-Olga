cb006ddb3f01158e89eee4b9fbb0790835e6d3db
Olga Mikhailova <foxx_fire@mail.ru>
2024-02-05 03:42:27 +0300

    Cells

--- iOS12-HW14-Mikhailova-Olga/Sorces/Cells/MediaTypesCollectionViewCell.swift
+++ iOS12-HW14-Mikhailova-Olga/Sorces/Cells/MediaTypesCollectionViewCell.swift
@@ -1,8 +1,95 @@
 //
 //  MediaTypesCollectionViewCell.swift
-//  iOS12-HW14-Mikhailova-Olga
+//  HW14
 //
-//  Created by mbpro2.0/16/512 on 05.02.2024.
+//  Created by mbpro2.0/16/512 on 04.02.2024.
 //
 
-import Foundation
+import UIKit
+
+class MediaTypesCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "Media"
+    
+    lazy var mediaImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    lazy var categoryLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = UIColor.systemBlue
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var countLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureCell(categoryName: String, imageName: String) {
+        categoryLabel.text = categoryName
+        mediaImageView.image = UIImage(named: imageName)
+    }
+    
+    private func setupHierarchy() {
+        backgroundColor = .white
+        addSubview(mediaImageView)
+        addSubview(categoryLabel)
+        addSubview(countLabel)
+    }
+    
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            mediaImageView.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            mediaImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 7),
+            mediaImageView.bottomAnchor.constraint(equalTo: bottomAnchor,constant: -10),
+            
+            categoryLabel.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+            categoryLabel.leadingAnchor.constraint(equalTo: mediaImageView.trailingAnchor, constant: 6),
+            categoryLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5),
+            
+            countLabel.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+            countLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5),
+            countLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10)
+            
+        ])
+    }
+    
+    //MARK: Configuration
+    
+    func configureCell(categoryName: String, count: String, imageName: String) {
+        categoryLabel.text = categoryName
+        countLabel.text = count
+        mediaImageView.image = UIImage(systemName: imageName)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.mediaImageView.image = nil
+        self.categoryLabel.text = nil
+        self.countLabel.text = nil
+    }
+}
+
+
--- iOS12-HW14-Mikhailova-Olga/Sorces/Cells/SharedAlbumsCollectionViewCel.swift
+++ iOS12-HW14-Mikhailova-Olga/Sorces/Cells/SharedAlbumsCollectionViewCel.swift
@@ -1,8 +1,92 @@
 //
-//  SharedAlbumsCollectionViewCel.swift
-//  iOS12-HW14-Mikhailova-Olga
+//  SharedAlbumsCollectionViewCell.swift
+//  HW14
 //
-//  Created by mbpro2.0/16/512 on 05.02.2024.
+//  Created by mbpro2.0/16/512 on 04.02.2024.
 //
 
-import Foundation
+import UIKit
+
+class SharedAlbumsCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "Shared"
+    
+    lazy var sharedImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 5
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleToFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    lazy var categoryLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var countLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupHierarchy() {
+        backgroundColor = .white
+        addSubview(sharedImageView)
+        addSubview(categoryLabel)
+        addSubview(countLabel)
+        
+    }
+    
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            sharedImageView.topAnchor.constraint(equalTo: topAnchor),
+            sharedImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            sharedImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            sharedImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            categoryLabel.topAnchor.constraint(equalTo: sharedImageView.bottomAnchor, constant: 5),
+            categoryLabel.leadingAnchor.constraint(equalTo: sharedImageView.leadingAnchor, constant: 5),
+            
+            countLabel.topAnchor.constraint(equalTo: categoryLabel.bottomAnchor, constant: 5),
+            countLabel.leadingAnchor.constraint(equalTo: sharedImageView.leadingAnchor, constant: 5)
+        ])
+    }
+    
+    //MARK: Configuration
+    
+    func configureCell(categoryName: String, count: String, imageName: String) {
+        categoryLabel.text = categoryName
+        countLabel.text = count
+        sharedImageView.image = UIImage(named: imageName)
+    }
+    
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.sharedImageView.image = nil
+        self.categoryLabel.text = nil
+        self.countLabel.text = nil
+    }
+}
+
+
